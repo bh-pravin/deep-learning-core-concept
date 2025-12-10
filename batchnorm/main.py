5e9e5972831347fc0d3e08e16277e0fceec10d8f
Pravin <haligondapravin@gmail.com>
2025-12-10 12:15:34 +0530

    using simple classifier from pytorch for experimenting batchnorm

---
+++ batchnorm/main.py
@@ -0,0 +1,93 @@
+import torch
+import torchvision
+import torch.optim as optim
+import torch.nn as nn 
+import torchvision.transforms as transforms
+import torch.nn.functional as F 
+
+cls_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+                                        root='data', 
+                                        train=True, 
+                                        download=True, 
+                                        transform=cls_transform
+                                        )
+
+testset = torchvision.datasets.CIFAR10(
+                                        root='data',
+                                        train=False,
+                                        download=True,
+                                        transform=cls_transform
+                                        )
+
+batch_size = 4
+
+train_loader = torch.utils.data.DataLoader(
+                                        dataset=trainset,
+                                        batch_size=batch_size,
+                                        shuffle=True
+                                        )
+
+test_loader = torch.utils.data.DataLoader(
+                                        dataset=testset,
+                                        batch_size=batch_size,
+                                        shuffle=False
+                                        )
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.fc1 = nn.Linear(in_features=16*5*5, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.fc3 = nn.Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, start_dim=1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x 
+    
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        if i == 10000:
+            import code; code.interact(local=locals())
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
